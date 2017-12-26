9c9133c5c74990a4475d9bc35e6695f6d14b5c2b
Andross2 <airbag15@gmail.com>
2017-12-26 16:42:52 -0500

    added finished bash script

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,27 @@
+#Begin script
 
+echo "How many files are in the current directory?"
+echo "Guess: "
+read guess
+
+function retrieve_file_number {
+	local num = $(ls -l | wc -l)-1
+	echo $num
+}
+
+correct_num = $(retrieve_file_number)
+
+while [[ $guess -ne $correct_num ]]
+do 
+	if [[ $guess -gt $correct_num ]]
+	then 
+		echo "Sorry, your guess was too high."
+	else 
+		echo "Sorry, your guess was too low."
+	fi
+	echo
+	echo "Guess again: "
+	read guess
+done
+
+echo "Good work. You guessed the right number! $correct_num." 
